5a2cd2f0700d12a62f9df7c855eb66eabd2fe8f7
Sandro Garcia <72823705+sandrovisky@users.noreply.github.com>
2023-05-21 00:10:42 -0300

    ensure RemoteAuthentication calls HttpClient with correct Body

--- app/lib/layers/domain/usecases/authentication.dart
+++ app/lib/layers/domain/usecases/authentication.dart
@@ -1,8 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import '../entities/account_entity.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({
-    required String user,
-    required String password,
+  Future<AccountEntity> auth({required AuthenticationParams params});
+}
+
+class AuthenticationParams {
+  final String user;
+  final String password;
+
+  AuthenticationParams({
+    required this.user,
+    required this.password,
   });
 }
--- app/test/data/usecases/remote_authentication_test.dart
+++ app/test/data/usecases/remote_authentication_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:app/layers/domain/usecases/usecases.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -6,15 +8,20 @@ class RemoteAuthentication {
   final HttpClient httpClient;
   final String url;
 
-  RemoteAuthentication({required this.httpClient, required this.url});
+  RemoteAuthentication({
+    required this.httpClient,
+    required this.url,
+  });
 
-  Future<void>? auth() async {
-    await httpClient.request(url: url, method: 'post');
+  Future<void>? auth(AuthenticationParams params) async {
+    final body = {'user': params.user, 'password': params.password};
+    await httpClient.request(url: url, method: 'post', body: body);
   }
 }
 
 abstract class HttpClient {
-  Future<void>? request({required String url, required String method});
+  Future<void>? request(
+      {required String url, required String method, Map? body});
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
@@ -29,13 +36,15 @@ void main() {
   );
 
   test('should call HttpCliente with correct values', () async {
-    await sut.auth();
+    final params = AuthenticationParams(
+        user: faker.person.firstName(), password: faker.internet.password());
+    await sut.auth(params);
 
     verify(
-      () => httpClient.request(
-        url: url,
-        method: 'post',
-      ),
+      () => httpClient.request(url: url, method: 'post', body: {
+        'user': params.user,
+        'password': params.password,
+      }),
     );
   });
 }
