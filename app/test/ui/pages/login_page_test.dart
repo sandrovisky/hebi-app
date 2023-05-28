eaf3c0effa20d7de5c4a3177389be62135be5303
Sandro Garcia <72823705+sandrovisky@users.noreply.github.com>
2023-05-28 04:05:42 -0300

    ensure LoginPage can calls authentication on form submit

--- app/test/ui/pages/login_page_test.dart
+++ app/test/ui/pages/login_page_test.dart
@@ -217,4 +217,18 @@ void main() {
       expect(button.onPressed, isNotNull);
     },
   );
+
+  testWidgets(
+    'should disables form button if form is invalid',
+    (WidgetTester tester) async {
+      await loadPage(tester);
+
+      isFormValidController!.add(false);
+      await tester.pump();
+
+      final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
+
+      expect(button.onPressed, isNull);
+    },
+  );
 }
